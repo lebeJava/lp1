dd705a03bfa83e72411315b3323172a3f070d2b2
Francisco Tamayo <lebe.java@gmail.com>
2019-04-11 04:15:17 -0500

    Pintar terminado

--- Basics/Ajedrez/main.cpp
+++ Basics/Ajedrez/main.cpp
@@ -20,47 +20,48 @@ int Calcular(int x, int y, int j) {
     if(restaY > 8) restaY = 8;
     return restaX*restaY;
 }
+
 int main()
 {
     int x, y, j;
     #if DEBUG
-        int T;
-        cin>>T;
-        while(T--)
-        {
-            cin >> x;
-            cin >> y;
-            cin >> j;
-            if(j >= 7) {
-                cout<< 64 <<endl;
-            }else{
-                cout<< Calcular(x, y, j) <<endl;
-            }
-        }
-    #else
-        cout << "Ingresa la posicion x (1-8): ";
+    int T;
+    cin>>T;
+    while(T--)
+    {
         cin >> x;
-        if(!x || x < 1 || x > 8) {
-            cout << "ERROR";
-            return 0;
-        }
-        cout << "Ingresa la posicion y (1-8): ";
         cin >> y;
-        if(!y || y < 1 || y > 8) {
-            cout << "ERROR";
-            return 0;
-        }
-        cout << "Ingresa el numero de jugadas: ";
         cin >> j;
-        if(!j || j < 0) {
-            cout << "ERROR";
-            return 0;
-        }
         if(j >= 7) {
-            cout << 64;
+            cout<< 64 <<endl;
         }else{
-            cout << Calcular(x, y, j);
+            cout<< Calcular(x, y, j) <<endl;
         }
+    }
+    #else
+    cout << "Ingresa la posicion x (1-8): ";
+    cin >> x;
+    if(!x || x < 1 || x > 8) {
+        cout << "ERROR";
+        return 0;
+    }
+    cout << "Ingresa la posicion y (1-8): ";
+    cin >> y;
+    if(!y || y < 1 || y > 8) {
+        cout << "ERROR";
+        return 0;
+    }
+    cout << "Ingresa el numero de jugadas: ";
+    cin >> j;
+    if(!j || j < 0) {
+        cout << "ERROR";
+        return 0;
+    }
+    if(j >= 7) {
+        cout << 64;
+    }else{
+        cout << Calcular(x, y, j);
+    }
     #endif
     return 0;
 }
