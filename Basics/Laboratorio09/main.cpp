eebec0da4c437f6b92bc5feccee233bae0fd3942
Francisco Tamayo <lebe.java@gmail.com>
2019-04-10 21:28:19 -0500

    Laboratorio 09 terminado

---
+++ Basics/Laboratorio09/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 7;
+    int b = 4;
+    if(a <= b)
+    {
+
+    }else
+    {
+        int t = a;
+        a = b;
+        b = t;
+    }
+    return 0;
+}
