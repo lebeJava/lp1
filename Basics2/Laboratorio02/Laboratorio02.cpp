3e532436cf4a2c5e5e86e43620fe1e606b2344fb
Francisco Tamayo <lebe.java@gmail.com>
2019-05-03 12:33:13 -0500

    Laboratorio 02 terminado

---
+++ Basics2/Laboratorio02/Laboratorio02.cpp
@@ -0,0 +1,12 @@
+int area (int length, int width);
+
+int main () {
+  int x0 = arena(7); // error : función no declarada
+  int x1 = area(7); // error : numero de argumentos
+  int x2 = area("seven", 2); // error: error en el tipo del primer argumento
+  return area(4, 4);
+}
+
+int area(int length, int width) {
+  return length*width;
+}
