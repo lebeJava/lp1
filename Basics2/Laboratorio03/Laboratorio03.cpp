427387ff735dc416f8e03bffffa0e0bc4d7c009a
Francisco Tamayo <lebe.java@gmail.com>
2019-05-03 12:36:03 -0500

    Laboratorio 03 terminado

---
+++ Basics2/Laboratorio03/Laboratorio03.cpp
@@ -0,0 +1,12 @@
+int area (int length, int width);
+
+int main () {
+  int x4 = arena(10, -7); // ok : pero ractángulo
+  int x5 = area(10.7, 9.3); // ok : pero llama a area(10, 9)
+  int x6 = area(100, 9999); // ok : pero trunca el resultado
+  return area(4, 4);
+}
+
+int area(int length, int width) {
+  return length*width;
+}
