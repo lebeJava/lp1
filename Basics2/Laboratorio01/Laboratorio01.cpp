8586a1a1c05d731c38cc5aafaac0c2b7d410d6fb
Francisco Tamayo <lebe.java@gmail.com>
2019-05-03 12:29:53 -0500

    Laboratorio 01 terminado

---
+++ Basics2/Laboratorio01/Laboratorio01.cpp
@@ -0,0 +1,13 @@
+int area (int length, int width);
+
+int main () {
+  int s1 = area(7; // error : falta)
+  int s1 = area(7) // error : falta;
+  Int s3 = area(7); // error : Int no es un tipo
+  int s4 = area(’7); // error : caracter(falta’)
+  return area(4, 4);
+}
+
+int area(int length, int width) {
+  return length*width;
+}
