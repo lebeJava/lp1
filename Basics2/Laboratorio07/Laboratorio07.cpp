0996ad7f848461006969bf83d09677b655173bcb
Francisco Tamayo <lebe.java@gmail.com>
2019-05-03 14:01:10 -0500

    Laboratorio 07 terminado

---
+++ Basics2/Laboratorio07/Laboratorio07.cpp
@@ -0,0 +1,19 @@
+class Bad_area {};
+int area(int length, int width) {
+  if(length <= 0 || width <= 0) throw Bad_area{};
+  return length*width;
+}
+
+int main() {
+  try {
+    int x = -1;
+    int y = 2;
+    int z = 4;
+    int area1 = area(x, y);
+    int area2 = framed_area(1, z);
+  }catch(exception & e) {
+    cerr << "¡Oops! Argumentos invalidos para area()" << endl;
+    return 1;
+  }
+  return 0;
+}
