e1980522e197c5b76374ca9c6a60da5d56593afd
Francisco Tamayo <lebe.java@gmail.com>
2019-05-03 13:52:02 -0500

    Laboratorio 05 terminado

---
+++ Basics2/Laboratorio05/Laboratorio05.cpp
@@ -0,0 +1,37 @@
+int area(int length, int width) {
+  return length*width;
+}
+
+const int frame_width = 2;
+//Calcula el area dentro de un marco
+int framed_area(int x, int y) {
+  return area(x-frame_width, y-frame_width);
+}
+
+int main() {
+  try {
+    int x = 1;
+    int y = 2;
+    {
+      if(x <= 0) throw runtime_error("x es negativo");
+      if(y <= 0) throw runtime_error("y es negativo");
+      int area1 = area(x, y);
+      //Si no separamos específicamente el error, sería:
+      if(x <= 0 || y <= 0) throw runtime_error("argumento no positivo");
+      int area1 = area(x, y)
+      if(z <= 2) throw runtime_error("argumento no positivo");
+      int area2 = framed_area(1, z);
+      if(y <= 2 || z <= 2) throw runtime_error("argumento no positivo");
+      int area3 = framed_area(y, z);
+      double ratio = double(area1)/area3;
+      if(1-frame_width <= 0 || z-frame_width <= 0) throw runtime_error("argumento negativo");
+      int area2 = framed_area(1, z);
+      if(y-frame_width <= 0 || z-frame_width <= 0) throw runtime_error("argumento negativo");
+      int area3 = framed_area(y, z);
+    }
+  }catch(exception & e) {
+    cerr << "Error: " << e.what() << endl;
+    return 1;
+  }
+  return 0;
+}
